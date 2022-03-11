eb7fd6bf3d2876b83b171cb7b857bfdd1831463d
SparrowLem <suslovaolesya99@gmail.com>
2022-03-11 16:44:13 +0300

    Chat

--- chat.js
+++ chat.js
@@ -1,28 +1,59 @@
 let chatContent = document.querySelector('.chat-content'); // Контейнер для сообщений
-var messages = chatContent.children; // Коллекция всех сообщений в контейнере
 let newMessageForm = document.querySelector('.chat-form'); // Форма и поле ввода текста
 let newMessageInput = document.querySelector('.chat-form-input'); 
 // Шаблон для сообщения
 let messageTemplate = document.querySelector('#message-template').content;
-let newMessageTemplate = messageTemplate.querySelector('.chat-message');
-
-newMessageForm.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-
-    // Получаем текст из поля ввода
-var messageText = newMessageInput.value;
-    // Клонируем шаблон сообщения
-var newMessage = newMessageTemplate.cloneNode(true);
-    // Добавляем текст введенный пользователем
-newMessage.children[1].textContent = messageText;
-    // Добавляем сообщение на страницу
-chatContent.appendChild(newMessage);
-    // Удаляем содержимое поля ввода
-newMessageInput.value = '';
-});
+//let newMessageTemplate = messageTemplate.querySelector('.chat-message');
+let comments = [];//
+//let comments = JSON.parse(localStorage.getItem('comments')) || [];
 
 
-   /*let newMessage = document.createElement('li'); 
-    newMessage.textContent = input.value;
-    input.value = '';
-    chat.append(newMessage); */
+if (localStorage.comments) {
+    // формируем переписку
+    localStorage.comments
+        .split('</p>,')
+        .map(p => chatContent.insertAdjacentHTML('beforeend', p))
+}
+
+function addMessage (evt) {    //функция отправки сообщений
+      evt.preventDefault()
+        let messageText = newMessageInput.value; // Получаем текст из поля ввода 
+        let template = `<p><span>${'User1'}</span> ${messageText}</p>`;
+
+    // добавляем шаблон в контейнер
+    chatContent.insertAdjacentHTML('afterbegin', template)
+    // сбрасываем значение инпута
+    newMessageInput.value = ''
+    // записываем сообщение в хранилище
+    //localStorage.comments = template
+    //comments.push(template)
+    //localStorage.setItem(comments, JSON.stringify(template));
+    // массив сообщений
+    // заполняем массив
+      
+    //comments.push(template);
+    
+    // записываем сообщение в хранилище
+    localStorage.comments = template
+}; 
+
+window.onbeforeunload = function (evt) { 
+	let warning = "Document 'foo' is not saved. ";
+	if (typeof evt == "undefined") {
+		evt = window.event;
+	}
+	if (evt) {
+		evt.returnValue = warning;
+         // заполняем массив
+         document.querySelectorAll('p').forEach(p => comments.push(p.outerHTML));
+        // записываем данные в хранилище
+        localStorage.comments = comments;
+	}
+	return warning;
+   
+}  
+
+newMessageForm.addEventListener('submit', addMessage); 
+
+//localStorage.clear();
+//comments = [];
