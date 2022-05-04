4c92bd98e53664cb301ded7df6fb01d0eca25184
SparrowLem <suslovaolesya99@gmail.com>
2022-05-04 12:49:09 +0300

    change

--- plugin.js
+++ plugin.js
@@ -1,12 +1,13 @@
 'use strict';
 
 (() => {
-	class SomeComponent extends videojs.getComponent('Component') {
+	class ChatContainer extends videojs.getComponent('Component') {
 		constructor(player, options) {
 			super(player, options);
+			this.addClass(options.customClass)
 			this.el().innerHTML = `
 			<div class="chat-messages" style="height: 80%; background: rgba(205, 214, 219, 0.3);
-    		overflow: scroll;"></div>
+			overflow: scroll;"></div>
 			<form action="" method="post">
 				<input type="text" required style="background: rgba(205, 214, 219, 0.3)"><button type="submit">Отправить</button>
 			</form>
@@ -17,35 +18,19 @@
 			position: absolute;
 			bottom: 21px;
 			left: 5px`;
+			//let messages = JSON.parse(localStorage.getItem('messages')) || [];
 		}
+
+		
 
 		// Этот метод вызывается самим videojs при монтировании компонента
 		createEl() {
 			// Созданный тут элемент будет родительским для этого компоннета. Его можно будет получить вызвав this.el()
 			return videojs.dom.createEl('div', { className: 'vjs-some-component chat-content' });
 		}
-	}
 
-	// Регистрирую компонент, чтобы videojs знал, что он существует
-	videojs.registerComponent('SomeComponent', SomeComponent);
-
-  //let ps = new SomeComponent();  
-
-	// Создаю плагин
-	class SomePlugin extends videojs.getPlugin('plugin') {
-		constructor(player, options) {
-			super(player, options);
-
-			// Плагин после того как в плеер происходит событие ready добавляет плееру класс vjs-some-plugin и добавляет дочерний компонент SomeComponent созданный ранее
-			player.on('ready', function() {
-                player.addClass(options.customClass)
-				player.addClass('vjs-some-plugin');
-				player.addChild('SomeComponent', options);
-
-                let messages = JSON.parse(localStorage.getItem('messages')) || [];
-                console.log(messages);
-
-				function historyChat() {
+		renderMessages() {
+			let messages = JSON.parse(localStorage.getItem('messages')) || [];
 					if (messages.length>0) {
 						for (let i = 0; i<messages.length; i++){
 								console.log(messages[i].message);
@@ -58,12 +43,17 @@
 				}
 
 				//функция отправки сообщений
-				function sendMessage() {
+		sendMessage() {
+			let messages = JSON.parse(localStorage.getItem('messages')) || [];
 					let form = document.querySelector('form'); 
 					console.log(form);
 					let chatContent = document.querySelector('.chat-content');
-					chatContent.style.display = 'block';
+					console.log(chatContent);
+					//chatContent.style.cssText = `display: block`;
 					let chatMessages = document.querySelector('.chat-messages'); 
+					chatContent.style.display = 'block';
+
+					console.log(chatMessages);
 				
 					form.addEventListener('submit', function (evt) { 
 						evt.preventDefault();
@@ -77,29 +67,47 @@
 						obj.name = 'User1';
 						obj.message = messageText;
 						messages.push(obj);
-							
+						console.log(obj);
 						inputText.value = '';
 					});
+					return messages;
 				};
-                
+	
+	};
+	// Регистрирую компонент, чтобы videojs знал, что он существует
+	videojs.registerComponent('ChatContainer', ChatContainer);
+
+	// Создаю плагин
+	class ChatPlugin extends videojs.getPlugin('plugin') {
+		constructor(player, options) {
+			super(player, options);
+
+			// Плагин после того как в плеер происходит событие ready добавляет плееру класс vjs-some-plugin и добавляет дочерний компонент SomeComponent созданный ранее
+			player.on('ready', function() {
+				//player.addClass(options.customClass)
+				player.addClass('vjs-some-plugin');
+				player.addChild('ChatContainer', options);
+
+				
 				let videoPlay = false;
+				//let ChatContainer = document.querySelector()
 				player.on('play', function() {
 					if (!videoPlay) {
-						historyChat()
-						sendMessage();
+						ChatContainer.prototype.renderMessages();
+						ChatContainer.prototype.sendMessage();
+						
 						videoPlay = true;
 					} return videoPlay;
 				});
-
-				console.log(videoPlay);
-
-							//функция отправки сообщения
+				
+				//функция отправки сообщения
 				if (videoPlay === true) {
-				console.log(form);
-				sendMessage();
-				historyChat();
-				};
+					console.log(form);
+					ChatContainer.prototype.sendMessage();
+					//ChatContainer.prototype.renderMessages();
+					};
 
+				let messages = JSON.parse(localStorage.getItem('messages')) || [];
 				//при закрытии окна сохранение сообщений в localStorage
 				window.onbeforeunload = function (evt) { 
 					let warning = "Document 'too' is not saved. ";
@@ -111,7 +119,6 @@
 							
 							localStorage.setItem('messages', JSON.stringify(messages));
 							console.log(localStorage);
-							//let users = JSON.parse(localStorage.getItem('messages'));
 						}
 						return warning;
 					
@@ -120,14 +127,14 @@
 		}
 	}
 	// Регистрирую плагин чтобы videojs знал о его существовании
-	videojs.registerPlugin('somePlugin', SomePlugin);
+	videojs.registerPlugin('chatPlugin', ChatPlugin);
 
 	// Инициализирую плеер
-    videojs('video-fm', {
+	videojs('video-fm', {
 		plugins: {
-			somePlugin: {
-                customClass: 'check' 
-            }
+			chatPlugin: {
+			customClass: 'check' 
+			}
 		}
 	});
 })();
