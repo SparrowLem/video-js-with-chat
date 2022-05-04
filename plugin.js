9aebbfff2ecdadd73d6ed83f9f3377870d91066e
SparrowLem <suslovaolesya99@gmail.com>
2022-05-04 17:20:27 +0300

    change

--- plugin.js
+++ plugin.js
@@ -4,7 +4,8 @@
 	class ChatContainer extends videojs.getComponent('Component') {
 		constructor(player, options) {
 			super(player, options);
-			this.addClass(options.customClass)
+			this.addClass(options.customClass);
+			
 			this.el().innerHTML = `
 			<div class="chat-messages" style="height: 80%; background: rgba(205, 214, 219, 0.3);
 			overflow: scroll;"></div>
@@ -18,10 +19,25 @@
 			position: absolute;
 			bottom: 21px;
 			left: 5px`;
-			//let messages = JSON.parse(localStorage.getItem('messages')) || [];
-		}
+			this.messages = JSON.parse(localStorage.getItem('messages')) || [];
+			player.on('play', () => this.renderMessages());
+			player.on('play', () => this.sendMessage());
 
-		
+			window.onbeforeunload = function (evt) { 
+				let warning = "Document 'too' is not saved. ";
+					if (typeof evt == "undefined") {
+						evt = window.event;
+					}
+					if (evt) {
+						evt.returnValue = warning;
+						
+						localStorage.setItem('messages', JSON.stringify(this.messages));
+						console.log(localStorage);
+					}
+					return warning;
+				
+			};
+		}
 
 		// Этот метод вызывается самим videojs при монтировании компонента
 		createEl() {
@@ -30,7 +46,7 @@
 		}
 
 		renderMessages() {
-			let messages = JSON.parse(localStorage.getItem('messages')) || [];
+			let messages = this.messages;
 					if (messages.length>0) {
 						for (let i = 0; i<messages.length; i++){
 								console.log(messages[i].message);
@@ -42,36 +58,37 @@
 					}
 				}
 
-				//функция отправки сообщений
-		sendMessage() {
-			let messages = JSON.parse(localStorage.getItem('messages')) || [];
-					let form = document.querySelector('form'); 
-					console.log(form);
-					let chatContent = document.querySelector('.chat-content');
-					console.log(chatContent);
-					//chatContent.style.cssText = `display: block`;
-					let chatMessages = document.querySelector('.chat-messages'); 
-					chatContent.style.display = 'block';
 
-					console.log(chatMessages);
-				
-					form.addEventListener('submit', function (evt) { 
-						evt.preventDefault();
+		sendMessage() {
+			//let messages = JSON.parse(localStorage.getItem('messages')) || [];
+			//let messages = this.messages;
+			let form = document.querySelector('form'); 
+			console.log(form);
+			let chatContent = document.querySelector('.chat-content');
+			console.log(chatContent);
+			let chatMessages = document.querySelector('.chat-messages'); 
+			chatContent.style.display = 'block';
+		
+			console.log(chatMessages);
 						
-						let obj = {};
-						let inputText = document.querySelector('input'); 
-						let messageText = inputText.value;
-						let template = `<p><span>${'User1'}</span> ${messageText}</p>`;
-						//template.style.cssText = `margin: 0;`
-						chatMessages.insertAdjacentHTML('afterbegin', template);
-						obj.name = 'User1';
-						obj.message = messageText;
-						messages.push(obj);
-						console.log(obj);
-						inputText.value = '';
-					});
-					return messages;
-				};
+			form.addEventListener('submit', function (evt) { 
+				evt.preventDefault();
+								
+				let obj = {};
+				let inputText = document.querySelector('input'); 
+				let messageText = inputText.value;
+				let template = `<p><span>${'User1'}</span> ${messageText}</p>`;
+				chatMessages.insertAdjacentHTML('afterbegin', template);
+				obj.name = 'User1';
+				obj.message = messageText;
+				//messages.push(obj);
+				this.messages.push(obj);
+				console.log(obj);
+				inputText.value = '';
+			});
+			return this.messages;
+			//return messages;
+		};
 	
 	};
 	// Регистрирую компонент, чтобы videojs знал, что он существует
@@ -87,42 +104,6 @@
 				//player.addClass(options.customClass)
 				player.addClass('vjs-some-plugin');
 				player.addChild('ChatContainer', options);
-
-				
-				let videoPlay = false;
-				//let ChatContainer = document.querySelector()
-				player.on('play', function() {
-					if (!videoPlay) {
-						ChatContainer.prototype.renderMessages();
-						ChatContainer.prototype.sendMessage();
-						
-						videoPlay = true;
-					} return videoPlay;
-				});
-				
-				//функция отправки сообщения
-				if (videoPlay === true) {
-					console.log(form);
-					ChatContainer.prototype.sendMessage();
-					//ChatContainer.prototype.renderMessages();
-					};
-
-				let messages = JSON.parse(localStorage.getItem('messages')) || [];
-				//при закрытии окна сохранение сообщений в localStorage
-				window.onbeforeunload = function (evt) { 
-					let warning = "Document 'too' is not saved. ";
-						if (typeof evt == "undefined") {
-							evt = window.event;
-						}
-						if (evt) {
-							evt.returnValue = warning;
-							
-							localStorage.setItem('messages', JSON.stringify(messages));
-							console.log(localStorage);
-						}
-						return warning;
-					
-				}; 
 			});
 		}
 	}
